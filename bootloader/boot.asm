8dca7b02173f64b7026665b8c2557e7b42c87196
Leo <38267386+randomlylelo@users.noreply.github.com>
2026-02-22 20:52:30 -0500

    hello shreshth is happy

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,22 +1,32 @@
 bits 16                 ; Tell NASM this is 16-bit code
+; What is 16 bit code, it can run on a 32 bit CPU.
 org 0x7c00              ; Tell NASM to start outputting stuff at offset 0x7c00
+; why this magic number. probs from the manual that the bootloader 
+; what does org do
 
+; what do these labels mean? what is boot, .loop? halt?
 boot:
     mov si, hello       ; Point si register to the 'hello' string
+; hello is more than 2 bytes (16 bits), is it just the address of "hello" fn below? how does assmebly compile here.; why to si?
     mov ah, 0x0e        ; 0x0e means 'Write Character in TTY mode' BIOS function
+; 
 .loop:
     lodsb               ; Load byte from address ds:si into al, increment si
     or al, al           ; Is al == 0 (null terminator)?
-    jz halt             ; If zero, jump to halt
+    jz .try             ; If zero, jump to halt
     int 0x10            ; Call BIOS interrupt 0x10 (Video Services)
     jmp .loop           ; Repeat the loop
 
-halt:
-    cli                 ; Clear interrupt flag
-    hlt                 ; Halt execution
+.try:
+    mov si, hello
+    jmp .loop
+
+;    cli                 ; Clear interrupt flag
+;    hlt                 ; Halt execution
 
 hello:
-    db "Hello world!", 0 ; The string to print, ending with a null terminator
+    db "Hello shresth!", 0 ; The string to print, ending with a null terminator
 
 times 510 - ($-$$) db 0 ; Pad remaining bytes with zeroes until 510 bytes
+; What is $ - $$? $ is our current pointer, $$ is the previous?
 dw 0xaa55               ; Magic bootloader number (marks the sector as bootable)
